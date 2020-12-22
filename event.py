68edbc02f75f63e9ace9ecf6b6e8c5d42b813823
john-montogmery2003 <johnmontogmery2003@gmail.com>
2020-12-22 14:31:45 +0000

    Add eventmanager

--- event.py
+++ event.py
@@ -5,3 +5,38 @@ class Event:
     """
     def __init__(self):
         self.name = 'Some event'
+
+
+class EndGame(Event):
+    """
+    This event is sent at the end of the game
+    """
+    def __init__(self):
+        self.name = 'End Game'
+
+class Tick(Event):
+    """
+    A tick
+    """
+    def __init__(self):
+        self.name = 'Tick'
+
+class Keyboard(Event):
+    """
+    Event for keyboard clicks
+    """
+    def __init__(self, key):
+        self.name = 'Keyboard'
+        self.key = key
+    def __str__(self):
+        return str(self.key)
+
+class Mouse(Event):
+    """
+    Event for mouse clicks
+    """
+    def __init__(self, posx, posy, type):
+        self.name = 'Mouse'
+        self.posx = posx
+        self.posy = posy
+        self.type = type
