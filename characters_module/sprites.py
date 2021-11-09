16ad008c62aa95659f127df55208e09b5764da9c
john-montgomery2003 <johnmontgomery2003@gmail.com>
2021-11-09 08:49:57 +0000

    fix critical error, removed needed functions

--- characters_module/sprites.py
+++ characters_module/sprites.py
@@ -52,3 +52,14 @@ def stretech_image(imagenmame, progression, rect=None):
         h, w = imagenmame.get_height(), imagenmame.get_width()
         return pygame.transform.scale(imagenmame, (w, h + progression ** 2)), h + progression ** 2
 
+# Load a whole bunch of images and return them as a list
+def getImages(sheet, rects):
+    "Loads multiple images, supply a list of coordinates"
+    return [getImage(sheet, rect) for rect in rects]
+
+# Load a whole strip of images
+def loadStrip(rect, image_count, sheet):
+    "Loads a strip of images and returns them as a list"
+    tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+            for x in range(image_count)]
+    return getImages(sheet, tups)
